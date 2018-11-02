defc7d9847e2ffe6150708cd81ab8272a52ec992
Johann150 <johann.galle@protonmail.com>
2018-11-02 21:40:37 +0100

    move mouse functions to separate file
    the draw.js file just got too big

--- draw.js
+++ draw.js
@@ -134,90 +134,12 @@ function setup(){
 		}
 	};
 	// mouse handlers
-	canvas.onmousedown=function(evt){
-		context.beginPath();
-		if(document.getElementById("erase").checked){
-			context.globalCompositeOperation="destination-out";
-			context.strokeStyle="rgba(0,0,0,1)";
-			context.lineWidth=eraseWidth;
-		}else{
-			context.globalCompositeOperation="source-over";
-			context.strokeStyle=penColor;
-			context.lineWidth=penWidth;
-		}
-		activePath={
-			color:context.strokeStyle,
-			gco:context.globalCompositeOperation,
-			width:context.lineWidth,
-			points:[]
-		};
-		drawing=true;
-		prevX=evt.clientX-canvas.offsetLeft;
-		prevY=evt.clientY-canvas.offsetTop;
-		activePath.points.push({
-			x:prevX,
-			y:prevY
-		});
-		// draw a single point
-		context.moveTo(prevX,prevY);
-		context.lineTo(prevX,prevY);
-		context.stroke();
-	};
-	canvas.onmousemove=function(evt){
-		if(drawing){
-			context.moveTo(prevX,prevY);
-			prevX=evt.clientX-canvas.offsetLeft;
-			prevY=evt.clientY-canvas.offsetTop;
-			activePath.points.push({
-				x:prevX,
-				y:prevY
-			});
-			context.lineTo(prevX,prevY);
-			context.stroke();
-		}
-		// set erasor cursor
-		var style=document.body.style;
-		style.setProperty("--erase-x",evt.clientX+"px");
-		style.setProperty("--erase-y",evt.clientY+"px");
-	};
-	canvas.onmouseup=function(evt){
-		if(typeof evt!=='undefined'&&activePath!=null){
-			// this is a real mouse handler call and not a delegation from the touch handler
-			evt.stopPropagation();
-			evt.preventDefault();
-			context.moveTo(prevX,prevY);
-			prevX=evt.clientX-canvas.offsetLeft;
-			prevY=evt.clientY-canvas.offsetTop+window.scrollY;
-			activePath.points.push({
-				x:prevX,
-				y:prevY
-			});
-		}
-		// save what was drawn
-		image.push(activePath);
-		saved=false;
-		// display undo button as normal
-		document.getElementById('undo').style.filter="";
-		// user drew sth new so empty redoStack
-		redoStack=[];
-		// grey-out the redo button
-		document.getElementById('redo').style.filter="brightness(50%)";
-		context.lineTo(prevX,prevY);
-		context.stroke();
-		drawing=false;
-	};
+	canvas.onmousedown=mousedown;
+	canvas.onmousemove=mousemove;
+	canvas.onmouseup=mouseup;
 	document.onmouseup=function(){
 		if(drawing){
-			// save what was drawn
-			image.push(activePath);
-			saved=false;
-			// display undo button as normal
-			document.getElementById('undo').style.filter="";
-			// user drew sth new so empty redoStack
-			redoStack=[];
-			// grey-out the redo button
-			document.getElementById('redo').style.filter="brightness(50%)";
-			drawing=false;
+			mouseup();
 		}
 	};
 	// touch handlers
