0af7c83133944d531626a666c092ab025c78f7b1
Johann150 <johann.galle@protonmail.com>
2019-02-05 21:38:25 +0100

    file opening ability
    just ported from the desktop version

--- docs/online/file.js
+++ docs/online/file.js
@@ -9,10 +9,60 @@ function fileOpen(){
 		}
 		var reader=new FileReader();
 		reader.onload=()=>{
-			var data=JSON.parse(reader.result);
-			console.log(data);
+			if(saved&&confirm("aktuelles Tafelbild wird überschrieben!")){
+				_fileRead(JSON.parse(reader.result));
+			}
 		};
 		reader.readAsBinaryString(f);
 	};
 	file.click();
+}
+
+function _fileRead(data){
+	// reset everything
+	setup();
+	// load data
+	bgColor=rgb2hex(data.bg);
+	if(data.image==null){
+		image=[];
+		document.getElementById('undo').style.filter="brightness(50%)";
+	}else{
+		image=data.image;
+		document.getElementById('undo').style.filter="";
+	}
+	if(data.width!=canvas.width){
+		// adjust for different screen size
+		var imgScale=canvas.width/data.width;
+		for(var obj in image){
+			for(var pt in obj.points){
+				pt.x*=imgScale;
+				pt.y*=imgScale;
+			}
+		}
+		resize(Math.max(data.height,data.height*imgScale));
+	}else if(data.height>canvas.height){
+		// canvas was enlarged downward
+		resize(data.height);
+	}
+	repaintAll();
+	redoStack=data.redoStack;
+	if(redoStack.length>0){
+		document.getElementById('redo').style.filter="";
+	}else{
+		document.getElementById('redo').style.filter="brightness(50%)";
+	}
+	document.body.style.backgroundColour=bgColor;
+	penWidth=data.penWidth;
+	document.getElementById('stroke').value=penWidth;
+	penColor=data.penColor;
+	context.strokeStyle=penColor;
+	document.body.style.setProperty("--pen-color",penColor);
+	eraseWidth=data.eraseWidth;
+	gridColor=data.gridColor;
+	document.body.style.setProperty("--grid-color",gridColor);
+	saved=true;
+	// make sure everything will be visible
+	for(var i=0;i<image.length;i++){
+		resize(Math.max(image[i].points.max(),height));
+	}
 }
